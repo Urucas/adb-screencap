d85dabb6cbee2875ef1f7c6c361785aa48ed9fcb
vrunoa <vruno.alassia@gmail.com>
2016-07-19 19:17:01 -0300

    suppport multiple devices using -d param

--- cli.js
+++ cli.js
@@ -2,7 +2,13 @@
 var screencap = require('./dist/index.js').default;
 var semafor = require('semafor');
 var logger = semafor();
+var argv = require('minimist')(process.argv.slice(2));
+if(argv["h"] || argv["help"]) {
+  console.log("Usage\n adb-screencap [-d device_id]");
+  return;
+}
+console.log(argv);
 screencap(function(err, path) {
     if(err) logger.fail(err);
     else logger.ok(path);
-});
+}, argv["d"]);
